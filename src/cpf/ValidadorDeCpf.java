1e465ef58e24415c75a60792be3d33486e0d1b3f
Vinicius B <vinicius_bezerra@hotmail.com>
2020-03-30 20:58:34 -0300

    codigo refatorado

--- src/cpf/ValidadorDeCpf.java
+++ src/cpf/ValidadorDeCpf.java
@@ -63,46 +63,6 @@ public class ValidadorDeCpf {
     }
 
     public boolean validadorDeCPF(String cpf) {
-        boolean verificaCpf = false;
-        int validador = 0;
-        int soma = 0;
-        for (int i = 0, j = 10; i < 9; i++, j--) {
-
-            validador = Character.digit(cpf.charAt(i), 10);
-            soma = soma + (validador * j);
-
-
-        }
-        int n1 = 0;
-        if (soma % 11 < 2) {
-            n1 = 0;
-        } else {
-            n1 = 11 - (soma % 11);
-            //cpf = String.valueOf(n1);
-        }
-        if (n1 == Integer.parseInt(String.valueOf(cpf.charAt(9)))) {
-            verificaCpf = true;
-        } else {
-            return verificaCpf;
-        }
-
-        soma = 0;
-        for (int i = 0, j = 11; i < 10; i++, j--) {
-
-            validador = Character.digit(cpf.charAt(i), 10);
-            soma = soma + (validador * j);
-        }
-        int n2 = 0;
-        if (soma % 11 < 2) {
-            n2 = 0;
-        } else {
-            n2 = 11 - (soma % 11);
-        }
-        if (!(n2 == Integer.parseInt(String.valueOf(cpf.charAt(10))))) {
-            verificaCpf = false;
-        }
-
-        return verificaCpf;
-
+        return validarPrimeiroVerificador(cpf) && validarSegundoValidador(cpf);
     }
 }
